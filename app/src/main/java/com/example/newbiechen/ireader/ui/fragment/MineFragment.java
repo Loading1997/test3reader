2f35d593ddf403f482a16c16943f97d0555bdcd5
loading1997 <1372059609@qq.com>
2021-07-15 18:40:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/newbiechen/ireader/ui/fragment/MineFragment.java
@@ -0,0 +1,25 @@
+package com.example.newbiechen.ireader.ui.fragment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.newbiechen.ireader.R;
+import com.example.newbiechen.ireader.ui.base.BaseFragment;
+
+
+public class MineFragment extends BaseFragment {
+
+    @Override
+    protected int getContentId() {
+        return R.layout.fragment_mine;
+    }
+}
+
+
